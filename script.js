e2307a41d6c61197a6fa1935a6971775828a50c2
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-17 13:55:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let timerDisplay=document.getElementById("timer");
+
+let curretDate=new Date();
+
+setInterval(function () {
+	curretDate=new Date();
+	timerDisplay.innerHTML=curretDate.toLocaleString();
+},1000)
